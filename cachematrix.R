4d4a4e9890bf9c9c26e75e953ef2f04d81c7aaa3
kdjokic <kdjokic@vup.hr>
2014-06-22 17:37:51 +0200

    Test
    Test

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,4 @@
-## Put comments here that give an overall description of what your
+## It works! Firs time on github, huh...Put comments here that give an overall description of what your
 ## functions do
 
 ## Write a short comment describing this function
